726c198faf9174c554bdffd0a87f7a26b7876626
wes lawler <wes.lawler2@gmail.com>
2018-02-08 18:09:48 -0600

    added ingredient-info component

---
+++ src/app/shoplist/ingredient-info/ingredient-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ingredient-info',
+  templateUrl: './ingredient-info.component.html',
+  styleUrls: ['./ingredient-info.component.css']
+})
+export class IngredientInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
