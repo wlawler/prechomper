1c8411cdad8b2966b6d507b3b6cac1c3e07fdde7
wes lawler <wes.lawler2@gmail.com>
2018-02-28 20:47:34 -0600

    individual recipe item template added

--- src/app/recipe-vault/recipe-list/recipe-list.component.ts
+++ src/app/recipe-vault/recipe-list/recipe-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import  {Recipe} from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
- recipes = [];
+ recipes: Recipe[]  = [
+   new Recipe('Test Recipe','this is simply at test','http://3olj7vg0kmp1zz2i82eav8b5-wpengine.netdna-ssl.com/wp-content/uploads/2018/02/Snows-BBQ-01-1024x717.jpg')
+ ];
   constructor() { }
 
   ngOnInit() {
